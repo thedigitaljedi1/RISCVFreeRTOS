91a4f2f1f88a9d6248759141ae3fe534cbe7f808
SubhamMishra <smishra@rhrk.uni-kl.de>
2019-06-24 16:52:04 +0200

    Initialisation

---
+++ build/app/main_full.d
@@ -0,0 +1,92 @@
+build/app/main_full.o build/app/main_full.o: full_demo/main_full.c \
+ ../../Source/include/FreeRTOS.h FreeRTOSConfig.h \
+ freedom-e-sdk/env/freedom-e300-hifive1/platform.h \
+ freedom-e-sdk/include/sifive/const.h \
+ freedom-e-sdk/include/sifive/devices/aon.h \
+ freedom-e-sdk/include/sifive/devices/clint.h \
+ freedom-e-sdk/include/sifive/devices/gpio.h \
+ freedom-e-sdk/include/sifive/devices/otp.h \
+ freedom-e-sdk/include/sifive/devices/plic.h \
+ freedom-e-sdk/include/sifive/devices/prci.h \
+ freedom-e-sdk/include/sifive/devices/pwm.h \
+ freedom-e-sdk/include/sifive/devices/spi.h \
+ freedom-e-sdk/include/sifive/devices/uart.h freedom-e-sdk/env/hifive1.h \
+ ../../Source/include/projdefs.h ../../Source/include/portable.h \
+ ../../Source/include/deprecated_definitions.h \
+ ../../Source/portable/GCC/RISC-V/portmacro.h \
+ ../../Source/include/mpu_wrappers.h ../../Source/include/task.h \
+ ../../Source/include/list.h ../../Source/include/timers.h \
+ ../../Source/include/task.h ../../Source/include/semphr.h \
+ ../../Source/include/queue.h ../../Demo/Common/include/dynamic.h \
+ ../../Demo/Common/include/blocktim.h \
+ ../../Demo/Common/include/GenQTest.h \
+ ../../Demo/Common/include/recmutex.h \
+ ../../Demo/Common/include/TimerDemo.h \
+ ../../Demo/Common/include/EventGroupsDemo.h \
+ ../../Demo/Common/include/TaskNotify.h freedom-e-sdk/env/encoding.h
+
+../../Source/include/FreeRTOS.h:
+
+FreeRTOSConfig.h:
+
+freedom-e-sdk/env/freedom-e300-hifive1/platform.h:
+
+freedom-e-sdk/include/sifive/const.h:
+
+freedom-e-sdk/include/sifive/devices/aon.h:
+
+freedom-e-sdk/include/sifive/devices/clint.h:
+
+freedom-e-sdk/include/sifive/devices/gpio.h:
+
+freedom-e-sdk/include/sifive/devices/otp.h:
+
+freedom-e-sdk/include/sifive/devices/plic.h:
+
+freedom-e-sdk/include/sifive/devices/prci.h:
+
+freedom-e-sdk/include/sifive/devices/pwm.h:
+
+freedom-e-sdk/include/sifive/devices/spi.h:
+
+freedom-e-sdk/include/sifive/devices/uart.h:
+
+freedom-e-sdk/env/hifive1.h:
+
+../../Source/include/projdefs.h:
+
+../../Source/include/portable.h:
+
+../../Source/include/deprecated_definitions.h:
+
+../../Source/portable/GCC/RISC-V/portmacro.h:
+
+../../Source/include/mpu_wrappers.h:
+
+../../Source/include/task.h:
+
+../../Source/include/list.h:
+
+../../Source/include/timers.h:
+
+../../Source/include/task.h:
+
+../../Source/include/semphr.h:
+
+../../Source/include/queue.h:
+
+../../Demo/Common/include/dynamic.h:
+
+../../Demo/Common/include/blocktim.h:
+
+../../Demo/Common/include/GenQTest.h:
+
+../../Demo/Common/include/recmutex.h:
+
+../../Demo/Common/include/TimerDemo.h:
+
+../../Demo/Common/include/EventGroupsDemo.h:
+
+../../Demo/Common/include/TaskNotify.h:
+
+freedom-e-sdk/env/encoding.h:
